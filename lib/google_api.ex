fd58537f7d4146ed764470906f4743a63df8c466
giovane <giovane.tomaz@redealumni.com>
2017-08-10 17:38:40 -0300

    Project Created

---
+++ lib/google_api.ex
@@ -0,0 +1,18 @@
+defmodule GoogleApi do
+  @moduledoc """
+  Documentation for GoogleApi.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> GoogleApi.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
