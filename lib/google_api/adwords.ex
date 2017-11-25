2b054b3d2e6d38a5944c4cbb5d8755ea918ae9a8
Jonathan Principe <jonathan.principe@gmail.com>
2017-11-25 08:56:40 -0500

    make adwords api version dynamic

--- lib/google_api/adwords.ex
+++ lib/google_api/adwords.ex
@@ -1,13 +1,13 @@
 defmodule GoogleApi.Adwords do
 
-  def client(client_customer_id) do
+  def client(client_customer_id, version \\ "v201710") do
     %GoogleApi.AdwordsClient{
       client_customer_id:   client_customer_id,
       client_id:            Application.get_env(:google_api, :oauth2)[:client_id],
       client_secret:        Application.get_env(:google_api, :oauth2)[:client_secret],
       developer_token:      Application.get_env(:google_api, :adwords)[:developer_token],
       user_agent:           Application.get_env(:google_api, :adwords)[:user_agent],
-      report_url:           "https://adwords.google.com/api/adwords/reportdownload/v201609"
+      report_url:           "https://adwords.google.com/api/adwords/reportdownload/#{version}"
     }
   end
 
