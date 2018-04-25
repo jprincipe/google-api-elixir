0e0f20728a8e12bb52a088305a241c57dbaed8fa
Jonathan Principe <jonathan.principe@gmail.com>
2018-04-25 14:38:24 -0400

    upgrade httpoison

--- mix.exs
+++ mix.exs
@@ -32,7 +32,7 @@ defmodule GoogleApi.Mixfile do
     [
       {:oauth2, "~> 0.9"},
       {:poison, "~> 3.0"},
-      {:httpoison, "~> 0.13.0"},
+      {:httpoison, "~> 1.1.1"},
       {:nimble_csv, "~> 0.1.0"}
     ]
   end
